3a9677b5e45f0819cd27b999d5b36e785f221929
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-06-30 18:48:53 +1000

    re_order program

--- execute.ps1
+++ execute.ps1
@@ -9,9 +9,11 @@ $destinationDir = "E:\GNAF\data\2024\MAY"
 $directory_path = $destinationDir -replace '\\', '/'
 
 $rsc = "H:\Program Files\R\R-4.3.2\bin\x64\Rscript.exe"
-$rspt_conv = "E:/GNAF/process_supply/parquet_convert.r"
-& $rsc --vanilla $rspt_conv $directory_path
 
 #remove agsg 2016 files
 $rspt_asgs_rem = "E:/GNAF/process_supply/remove_asgs_2016.r"
 & $rsc --vanilla $rspt_asgs_rem $directory_path
+
+#convert the parquet files
+$rspt_conv = "E:/GNAF/process_supply/parquet_convert.r"
+& $rsc --vanilla $rspt_conv $directory_path
