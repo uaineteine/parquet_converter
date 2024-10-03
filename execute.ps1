8858194a096500a2a167d55cce4ce98f47343624
Daniel <39141263+uaineteine@users.noreply.github.com>
2024-10-03 23:51:10 +1000

    Update execute.ps1

--- execute.ps1
+++ execute.ps1
@@ -1,9 +1,4 @@
-# Define variables for the zip file path and destination directory
-$zipFilePath = "E:\GNAF\data\g-naf_may24_allstates_gda2020_psv_1015.zip"
 $destinationDir = "E:\GNAF\data\2024\MAY"
-
-# Call the unzip script with the defined variables
-& .\unpack_zip.ps1 -zipFilePath $zipFilePath -destinationDir $destinationDir
 
 # Execute the R script with the directory path argument
 $directory_path = $destinationDir -replace '\\', '/'
